ccf77638238742f3ad7c9dfca987b5748e4a8dab
Joshua Hegedus <josh.hegedus@outlook.com>
2024-02-22 20:36:52 +0100

    added rolling ball

--- U_1/rolling-ball/Assets/Scripts/PlayerScript.cs
+++ U_1/rolling-ball/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,10 +11,17 @@ public class PlayerScript : MonoBehaviour
 
     private bool canJump = true;
 
+    private float level = 1;
+    private float bestLevel = 1;
+
+    public Text LevelText;
+    public Text BestLevelText;
+
     // Start is called before the first frame update
     void Start()
     {
-      
+        BestLevelText.text = $"Best level: {bestLevel}";
+        LevelText.text = $"Level: {level}";
     }
 
     // Update is called once per frame
@@ -24,25 +33,44 @@ public class PlayerScript : MonoBehaviour
         transform.Translate(-5 * Time.deltaTime, 0, 0);
       }
 
-
       if(Input.GetKey("d")){
         transform.Translate(5 * Time.deltaTime, 0, 0);
       }
+
+      transform.Translate(Input.acceleration.x, 0, 0);
 
       if(Input.GetMouseButtonDown(0) && canJump){
         canJump = false;
         GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpForce, 0));
       }
 
-      if(transform.position.y < 0){
-        transform.position = new Vector3(0, 1, 1);
+      if(transform.position.y < -10){
+        ResetGame();
       }
+    }
+
+    private void ResetGame() {
+        transform.position = new Vector3(0, 1, 1);
+        Speed = 20f;
+        level = 1;
+        LevelText.text = $"Level: {level}";
     }
 
     private void OnCollisionEnter(Collision collision){
       canJump = true;
       if(collision.gameObject.tag == "Obstacle"){
+        ResetGame();
+      }
+
+      if(collision.gameObject.tag == "Finish") {
         transform.position = new Vector3(0, 1, 1);
+        Speed += 5;
+        level += 1;
+        if (level > bestLevel) {
+          bestLevel = level;
+        }
+        BestLevelText.text = $"Best level: {bestLevel}";
+        LevelText.text = $"Level: {level}";
       }
     }
 }
